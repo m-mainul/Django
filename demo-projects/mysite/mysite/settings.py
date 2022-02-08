df95c42e13f4f43326382ffee1697de67ff32af1
Mainul Hasan <moh.mainul.hasan@gmail.com>
2022-02-08 09:29:15 +0600

    Add database configuration

--- demo-projects/mysite/mysite/settings.py
+++ demo-projects/mysite/mysite/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'OPTIONS': {
+    #         'read_default_file': 'databases/mysql/mysql.cnf',
+    #     },
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_site',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+    },
 }
 
 
